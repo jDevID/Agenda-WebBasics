049edbc36c4d423a6939259a1fa7aeec4327a51c
jDevID <dav.botton@gmail.com>
2023-05-05 14:59:29 +0200

    created list_appointments_view.php. Contains basic commands to manipulate a calendar brought by a Js script at location /public/js/calendar.js. Usage of JQuery for HTML manipulation and display of nice readable calendar style tabs.

---
+++ views/list_appointments_view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AgendApp | Rendez-vous</title>
+    <!-- TODO: un peu de style pour la lisibilité
+    <link rel="stylesheet" href="../public/css/calendar.css"> -->
+    <!-- JQuery librairie Js, permet la manipulation aisée de documents HTML -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="../public/js/calendar.js"></script> <!-- le script js pour l'affichage du calendrier -->
+</head>
+
+<!-- Contrôles du calendrier -->
+<body>
+    <button id="prevMonth">Previous Month</button>
+    <button id="nextMonth">Next Month</button>
+    <div id="calendar"></div>
+</body>
+</html>
