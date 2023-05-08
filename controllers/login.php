0d95ebdddd8e8ad0132f5c71952abc68ca3941d1
jDevID <dav.botton@gmail.com>
2023-05-08 19:09:30 +0200

    updated login.php - minor fix

--- controllers/login.php
+++ controllers/login.php
@@ -1,8 +1,6 @@
 <?php
 // ob_start(); // https://www.php.net/manual/en/function.ob-start.php
-
 require_once('../models/init.php');
-
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     require('../views/login_view.php');
@@ -12,19 +10,16 @@ else {
     $password = $_POST['password'];
 
     $user = new User();
-    // TODO : gérer les méthodes de classe: inscription et log-in
     $result = $user->login($username, $password);
 
     if ($result) {
         session_start();
-        $_SESSION['username'] = $username;
-        // header vers la page Vue Générale Calendrier
-        header('Location: list_appointments.php');
+        $_SESSION['username'] = $result['username'];
+        header('Location: ./list_appointments.php');
         exit();
     } else {
         // si le log-in fails, redirection
         require('../views/login_view.php');
     }
 }
-
 // ob_end_flush();
