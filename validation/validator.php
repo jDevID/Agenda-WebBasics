e91c0902ffee78b24a5f8bf90e8e4ed9d0b1b76d
jDevID <dav.botton@gmail.com>
2023-05-09 13:42:14 +0200

    updated validator.php - small fix in regex

--- validation/validator.php
+++ validation/validator.php
@@ -30,7 +30,7 @@ function validate_date_format($date) {
 }
 
 function validate_hour_format($hour) {
-    return preg_match('/^(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d$/', $hour);
+    return preg_match('/^(?:[01]\d|2[0-3]):[0-5]\d$/', $hour);
 }
 
 function validate_hour_range($start_hour, $end_hour): bool {
