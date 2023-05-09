1ef58e3b362528569bf9312c5dfebc035880e091
jDevID <dav.botton@gmail.com>
2023-05-09 23:49:58 +0200

    deleted list_appointments.php

--- controllers/list_appointments.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'session_check.php';
-require_once '../models/Rendezvous.class.php';
-
-$rendezvous = new Rendezvous();
-$data = $rendezvous->getAllRendezvous();
-
-require('../views/list_appointments_view.php');
