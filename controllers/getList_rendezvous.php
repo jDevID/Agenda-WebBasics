f3126fbc288e31d76a261d7eca96160451802c7c
jDevID <dav.botton@gmail.com>
2023-05-10 00:05:01 +0200

    updated getList_rendezvous.php, minor added wrap on json and allows now to display the list of rendez-vous

--- controllers/getList_rendezvous.php
+++ controllers/getList_rendezvous.php
@@ -4,12 +4,18 @@ header('Content-Type: application/json');
 require_once 'session_check.php';
 require_once '../models/Rendezvous.class.php';
 
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$year = isset($_GET['year']) ? (int)$_GET['year'] : date("Y");
+$month = isset($_GET['month']) ? (int)$_GET['month'] : date("m");
+
 $rendezvous = new Rendezvous();
-$data = $rendezvous->getAllRendezvous();
-echo json_encode($data);
+$data = $rendezvous->getRendezvousData($year, $month);
+
+// wrap le json payload du rdv
+header('Content-Type: application/json');
+// dans l'array $data pour vérifier validité
+echo json_encode(['status' => 'success', 'data' => $data]);
 
