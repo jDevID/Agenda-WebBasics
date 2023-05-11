fcd93474ffa70500aee80ad332f1cbb8c65646e5
jDevID <dav.botton@gmail.com>
2023-05-11 16:56:35 +0200

    updated main view - Restructured script js. Now clean +  renaming

--- views/list_appointments_view.php
+++ views/list_appointments_view.php
@@ -4,19 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AgendApp | Rendez-vous</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="../public/js/formulaire.js"></script>
+    <script src="../public/js/listeRendezvous.js"></script>
+    <script src="../public/js/calendrier.js"></script>
     <link rel="stylesheet" href="../public/css/calendar.css">
     <!-- JQuery librairie Js, permet la manipulation aisée de documents HTML -->
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 </head>
 
-<!-- Contrôles du calendrier -->
+
 <body>
 <div id="calendarContainer" class="calendar-container">
     <div id="calendar"></div>
     <div class="calendar-controls">
-        <button id="prevMonth">Précédent</button>
-        <button id="nextMonth">Suivant</button>
+        <button id="btn_moisPrecedentId">Précédent</button>
+        <button id="btn_moisSuivantId">Suivant</button>
     </div>
 </div>
 
@@ -25,7 +27,8 @@
     <div class="left-column">
         <div id="rendezvousForm" class="rendezvous-form">
             <h2>Edition Rendez-vous</h2>
-            <form id="rendezvousFormElem" action="../controllers/save_rendezvous.php" method="post">
+
+            <form id="rendezvousFormElem" action="../controllers/crud_rendezvous.php" method="post">
                 <input type="hidden" name="id" id="id">
                 <input type="hidden" name="action" id="action" value="save">
                 <label for="name">Nom:</label>
@@ -44,11 +47,12 @@
                 <input type="date" name="date" id="date" required>
                 <br>
 
-                <button type="submit">Sauver</button>
+                <button type="submit" id="saveUpdateBtn" >Sauver</button>
                 <button type="button" id="deleteBtn">Supprimer</button>
-                <button type="button" id="congeBtn">Congé</button>
+                <button type="submit" id="congeBtn">Congé</button>
             </form>
             <div id="formError" class="alert alert-danger"></div>
+
         </div>
     </div>
 
@@ -60,86 +64,31 @@
 
 </div>
 
-<!--
-AJAX pour gérer la sauvegarde de RDV en DB  !
--->
+
 <script>
-    function loadRendezvousList() {
-        $.ajax({
-            type: 'GET',
-            url: '../controllers/getList_rendezvous.php',
-            dataType: 'json',
-            success: function (response) {
-                if (response.status === 'success') {
-                    const rendezvousList = response.data;
-                    $('#rendezvousList').empty(); // Clear the list before appending new items
 
-                    rendezvousList.forEach(function (rendezvous) {
-                        // Create a list item for each rendezvous and append it to the rendezvous list
-                        const listItem = $('<li>')
-                            .text(rendezvous.name + ' - ' + rendezvous.date + ' - ' + rendezvous.start_hour + ' - ' + rendezvous.end_hour)
-                            .data('rendezvous', rendezvous)  // set le data du rdv a chaque item
-                            .appendTo('#rendezvousList');
-                    });
-                } else {
-                    console.error('Error occurred while fetching rendezvous list:', response.message);
-                }
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                console.error('Error occurred while fetching rendezvous list:', textStatus, errorThrown);
-            }
-        });
-    }
+    let calendar;
+    let formulaire;
+    let listeRendezvous;
+    // Assignation des classes aux id dans la vue
+    document.addEventListener("DOMContentLoaded", function () {
+        // L'ordre a de l'importance selon les dépendances de chaque component
+        formulaire = new Formulaire("rendezvousFormElem");
+        listeRendezvous = new ListeRendezvous("rendezvousList");
+        calendar = new Calendrier("calendar");
 
-    $(document).ready(function () {
-        const calendar = new Calendar("calendar", "rendezvousFormElem");
-        loadRendezvousList(); // Load the rendezvous list when the page is loaded
+        listeRendezvous.assignerDependances(calendar, formulaire);
+        calendar.assignerDependances(listeRendezvous, formulaire);
 
-        $('#rendezvousList').on('click', 'li', function () {
-            const rendezvousData = $(this).data('rendezvous');
-            calendar.fillFormFields(new Date(rendezvousData.date), rendezvousData);
-            $('#action').val('update');
-            $('#id').val(rendezvousData.id);
-        });
+        formulaire.initialize();
+        listeRendezvous.initialize();
+        listeRendezvous.refreshRendezvousListe();
 
-        function showError(message) {
-            $('#formError').text(message).show();
-        }
-
-        function clearError() {
-            $('#formError').hide();
-        }
-
-        $('#rendezvousFormElem').on('submit', function (e) {
-            e.preventDefault();
-
-            if (!this.checkValidity()) {
-                showError('Please fill out all required fields correctly.');
-                return;
-            }
-
-            $.ajax({
-                type: 'POST',
-                url: '../controllers/save_rendezvous.php',
-                data: $(this).serialize(),
-                success: function (response) {
-                    if (response.status === 'success') {
-                        console.log('Rendez-vous sauvé:', response);
-                        calendar.clearFormInputs(); // Clear the form inputs after successful save
-                        loadRendezvousList(); // Reload the rendezvous list after a successful save/update/delete
-                    } else {
-                        showError(response.message);
-                    }
-                },
-
-            });
-        });
-        $('#deleteBtn').on('click', function () {
-            $('#action').val('delete');
-            $('#rendezvousFormElem').submit();
-        });
+        calendar.initCalendrier();
+        calendar.cycleMoisAnnee();
     });
+
 </script>
-<script src="../public/js/calendar.js"></script> <!-- le script js pour l'affichage du calendrier -->
+
 </body>
 </html>
