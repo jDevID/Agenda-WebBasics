6af3a75d018b36f44e95cc1a28cb38ff486a79ed
jDevID <dav.botton@gmail.com>
2023-05-12 14:39:01 +0200

    updated ddl.sql - created table client + FK

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -13,13 +13,21 @@ CREATE TABLE IF NOT EXISTS users (
     password VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB;
 
+CREATE TABLE IF NOT EXISTS client (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+) ENGINE=InnoDB;
+
 CREATE TABLE IF NOT EXISTS rendezvous (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
+    client_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     description TEXT,
     date DATE NOT NULL,
     start_hour TIME NOT NULL,
     end_hour TIME NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY  (client_id) REFERENCES client(id)
 ) ENGINE=InnoDB;
