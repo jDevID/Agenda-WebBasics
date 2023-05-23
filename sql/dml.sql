8127fa114c0353381deb8dcd037d0acf47c9e5e7
jDevID <dav.botton@gmail.com>
2023-05-23 17:47:36 +0200

    minor fix in dml script

--- sql/dml.sql
+++ sql/dml.sql
@@ -22,22 +22,22 @@ VALUES (1, 'John Smith', 'john@example.com'),
        (8, 'Sophia Anderson', 'sophia@example.com');
 
 INSERT INTO rendezvous (user_id, client_id, name, description, date, start_hour, end_hour)
-VALUES (1, 1, 'John Smith', 'Discuss project details', '2023-05-16', '09:00:00', '10:00:00'),
-       (1, 2, 'Jane Doe', 'Review marketing strategy', '2023-05-20', '09:00:00', '10:00:00'),
-       (1, 3, 'David Johnson', 'Provide software demo', '2023-05-22', '09:00:00', '10:00:00'),
+VALUES (1, 1, 'John Smith', 'Discuss project details', '2023-06-16', '09:00:00', '10:00:00'),
+       (1, 2, 'Jane Doe', 'Review marketing strategy', '2023-06-20', '09:00:00', '10:00:00'),
+       (1, 3, 'David Johnson', 'Provide software demo', '2023-06-22', '09:00:00', '10:00:00'),
        (1, 4, 'Emily Wilson', 'Discuss budget allocation', '2023-05-25', '09:00:00', '10:00:00'),
        (1, 5, 'Michael Brown', 'Finalize partnership agreement', '2023-05-26', '09:00:00', '10:00:00'),
        (1, 6, 'Olivia Davis', 'Brainstorm new ideas', '2023-05-29', '09:00:00', '10:00:00'),
        (1, 7, 'Daniel Martinez', 'Present sales report', '2023-06-05', '09:00:00', '10:00:00'),
        (1, 8, 'Sophia Anderson', 'Discuss HR policies', '2023-06-10', '09:00:00', '10:00:00'),
-       (1, 1, 'John Smith', 'Review project progress', '2023-06-20', '09:00:00', '10:00:00'),
+       (1, 1, 'John Smith', 'Review project progress', '2023-06-20', '10:00:00', '11:00:00'),
        (1, 2, 'Jane Doe', 'Discuss upcoming events', '2023-06-25', '09:00:00', '10:00:00');
 
 INSERT INTO conge (date)
-VALUES ('2023-05-17'),
-       ('2023-05-18'),
-       ('2023-05-19'),
-       ('2023-05-21'),
-       ('2023-05-23'),
+VALUES ('2023-06-17'),
+       ('2023-06-18'),
+       ('2023-06-19'),
+       ('2023-06-21'),
+       ('2023-06-23'),
        ('2023-05-24');
 
