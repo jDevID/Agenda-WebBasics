63bff104c4df3e48677fbbc33dc1279e6060f453
jDevID <dav.botton@gmail.com>
2023-05-29 22:04:03 +0200

    created UserFactory.php

---
+++ models/UserFactory.php
@@ -0,0 +1,47 @@
+<?php
+require_once('../models/Toast.class.php');
+require_once('User.class.php');
+
+class UserFactory
+{
+
+    // Crée un nouvel utilisateur après validation
+    public function createUser(int    $id = -1,
+                               string $username = '',
+                               string $password = '',
+                               string $role = ''): User
+    {
+        $this->validateUserDetails($username, $password, $role);
+        $this->validateUserRole($role);
+
+        // Hache le mot de passe
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        return new User($id, $username, $hashedPassword, $role);
+    }
+
+    public function createUserForLogin(string $username = '', string $password = ''): User
+    {
+        $this->validateUserDetails($username, $password);
+        return new User(-1, $username, $password, '');
+    }
+
+    private function validateUserDetails(string $username, string $password): void
+    {
+        if (strlen($username) < 4 || strlen($username) > 25 || preg_match('/\d/', $username) || preg_match('/[^\p{L} ]/u', $username)) {
+            throw new Exception('Le nom d\'utilisateur doit comporter entre 4 et 25 caractères et ne peut pas contenir de symboles ou de nombres.');
+        }
+
+        // Validate password
+        if (strlen($password) < 6 || strlen($password) > 20 || !preg_match('/[A-Z]/', $password) || !preg_match('/\d/', $password) || preg_match('/[<>=\/\\\]/', $password) || preg_match('/\s/', $password)) {
+            throw new Exception('Le mot de passe doit comprendre entre 6 et 20 caractères, inclure au moins une lettre majuscule, un chiffre et ne peut pas contenir d\'espace, <, >, \', =, /, \\ .');
+        }
+    }
+
+    private function validateUserRole(string $role): void
+    {
+        // Here goes your role validation logic
+    }
+}
+
+?>
