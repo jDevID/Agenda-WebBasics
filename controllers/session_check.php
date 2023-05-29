a041233a67f6ae23afc957899c750fb436d30da5
jDevID <dav.botton@gmail.com>
2023-05-29 22:10:20 +0200

    deleted useless files

--- controllers/session_check.php
+++
@@ -1,9 +0,0 @@
-<?php
-// start la session quand appelé
-session_start();
-
-// redirection si non-identifié
-if (!isset($_SESSION['username'])) {
-    header('Location: login.php');
-    exit();
-}
