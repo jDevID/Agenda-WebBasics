9240cf81a1a1ac87dd4be75e508916dffd8f13c1
jDevID <dav.botton@gmail.com>
2023-05-29 22:13:36 +0200

    completed add_client.php

--- controllers/add_client.php
+++ controllers/add_client.php
@@ -11,3 +11,32 @@ error_reporting(E_ALL);
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
+
+if (!isset($_SESSION['username'])) {
+    header('Location:login.php');
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $role = $_POST['role'];
+
+    $userDAL = new UserDAL();
+    $userFactory = new UserFactory($userDAL);
+
+    try {
+        $user = $userFactory->createUser(-1, $username, $password, $role);
+
+        if ($userDAL->register($user)) {
+            Toast::throwMessage('Client created successfully.', 'success');
+            header('Location: ../views/main_view.php');
+            exit;
+        }
+    } catch (Exception $e) {
+        Toast::throwMessage('Caught exception: ' . $e->getMessage(), 'error');
+        header('Location: ../views/main_view.php');
+        exit;
+    }
+}
+?>
