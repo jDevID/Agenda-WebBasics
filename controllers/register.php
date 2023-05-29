b80311ec83b8458c9438a91738345500bf226ec7
jDevID <dav.botton@gmail.com>
2023-05-29 22:12:42 +0200

    completed register.php

--- controllers/register.php
+++ controllers/register.php
@@ -12,15 +12,41 @@ require_once '../models/UserFactory.php';
 require_once '../models/Toast.class.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_ADD_SLASHES);
+    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_ADD_SLASHES);
 
-    $user = new User();
-    $user->register($username, $password);
 
-    header('Location: login.php');
-    exit();
+    $userDAL = new UserDAL();
+    $userFactory = new UserFactory();
 
+    try {
+        // validate the user details
+        $user = $userFactory->createUser(-1, $username, $password, 'client');
+
+        if (!$userDAL->isUsernameUnique($username)) {
+            Toast::throwMessage('Username is already taken. Please choose another.', 'error');
+            header('Location: ../views/register_view.php');
+            exit();
+        }
+        // now register the user
+        $result = $userDAL->register($user);
+
+        $registrationSuccessful = $userDAL->register($user);
+
+        if ($registrationSuccessful) {
+            Toast::throwMessage('Inscription réussie. Vous pouvez maintenant vous connecter.', 'success');
+            header('Location: ../views/login_view.php');
+            exit();
+        } else {
+            Toast::throwMessage('Erreur durant le processus d\'inscription.', 'error');
+            header('Location: ../views/register_view.php');
+            exit();
+        }
+    } catch (Exception $e) {
+        Toast::throwMessage($e->getMessage(), 'error');
+        header('Location: ../views/register_view.php');
+        exit();
+    }
 } else {
     require('../views/register_view.php');
 }
