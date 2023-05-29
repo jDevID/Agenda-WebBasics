89ae09210c4ee0563d9edfb524a94ae258643a74
jDevID <dav.botton@gmail.com>
2023-05-29 22:10:54 +0200

    created list_client_async.php

---
+++ controllers/list_client_async.php
@@ -0,0 +1,42 @@
+<?php
+ob_start();
+
+require_once('../models/init.php');
+require_once('../models/UserFactory.php');
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+if (!isset($_SESSION['username'])) {
+    header('Location:login.php');
+    exit();
+}
+
+$userDAL = new UserDAL();
+$userFactory = new UserFactory($userDAL);
+
+$clients = $clientDAL->getAllUsers($clientFactory);
+ob_clean();
+
+header('Content-Type: application/xml');
+echo '<?xml version="1.0" encoding="UTF-8" ?>';
+echo '<clientList>';
+foreach ($clients as $client) {
+    if ($client !== null) {
+        $id = $client->getId();
+        $name = $client->getUsername();
+
+        echo '<client>';
+        echo "  <id>$id</id>";
+        echo "  <username>$name</username>";
+        echo '</client>';
+    }
+}
+echo '</clientList>';
+?>
+
+
