98c5c433825e9b54c3c6c3a21d2b521fac945c69
jDevID <dav.botton@gmail.com>
2023-06-04 13:50:33 +0200

    updated User Factory, server-side validation

--- models/UserFactory.php
+++ models/UserFactory.php
@@ -12,36 +12,34 @@ class UserFactory
     }
 
     // Crée un nouvel utilisateur après validation
-    public function createUser(int    $id = -1,
-                               string $username = '',
-                               string $password = '',
-                               string $role = ''): User
+    public function createUser(int $id = -1, string $username = '', string $password = '', string $role = '', bool $checkUsername = true, bool $passwordValidationRequired = true): User
     {
-        $this->validateUserDetails($username, $password);
-        $this->validateUserRole($role);
-        $this->validateUsernameAvailability($username);
+        $this->validateUserDetails($username, $password, $passwordValidationRequired);
 
-        // Hache le mot de passe
+        if ($checkUsername) {
+            $this->validateUsernameAvailability($username);
+        }
+
+        // Always hash the password when creating a user
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
         return new User($id, $username, $hashedPassword, $role);
     }
-
     public function createUserForLogin(string $username = '', string $password = ''): User
     {
-        $this->validateUserDetails($username, $password);
+        $this->validateUserDetails($username, $password, false);
         return new User(-1, $username, $password, '');
     }
-
-    private function validateUserDetails(string $username, string $password): void
+    private function validateUserDetails(string $username, string $password, bool $passwordValidationRequired): void
     {
         if (strlen($username) < 4 || strlen($username) > 25 || preg_match('/\d/', $username) || preg_match('/[^\p{L} ]/u', $username)) {
             throw new Exception('Le nom d\'utilisateur doit comporter entre 4 et 25 caractères et ne peut pas contenir de symboles ou de nombres.');
         }
 
-        // Validate password
-        if (strlen($password) < 6 || strlen($password) > 20 || !preg_match('/[A-Z]/', $password) || !preg_match('/\d/', $password) || preg_match('/[<>=\/\\\]/', $password) || preg_match('/\s/', $password)) {
-            throw new Exception('Le mot de passe doit comprendre entre 6 et 20 caractères, inclure au moins une lettre majuscule, un chiffre et ne peut pas contenir d\'espace, <, >, \', =, /, \\ .');
+        if ($passwordValidationRequired) {
+            if (strlen($password) < 6 || strlen($password) > 20 || !preg_match('/[A-Z]/', $password) || !preg_match('/\d/', $password) || preg_match('/[<>=\/\\\]/', $password) || preg_match('/\s/', $password)) {
+                throw new Exception('UserFactory -> Le mot de passe doit comprendre entre 6 et 20 caractères, inclure au moins une lettre majuscule, un chiffre et ne peut pas contenir d\'espace, <, >, \', =, /, \\ .');
+            }
         }
     }
 
